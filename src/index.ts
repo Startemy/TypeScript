a2404b8c1932d6b9668b9c3509571f047ab812c8
Artem Alaev <alayev7@gmail.com>
2022-10-03 16:43:08 +0300

    Попытка сделать 3 задание

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,22 @@ import { renderSearchStubBlock } from './search-results.js'
 import { renderUserBlock } from './user.js'
 import { renderToast } from './lib.js'
 
+let dateEntery: string
+let dateExit: string
+
 window.addEventListener('DOMContentLoaded', () => {
   renderUserBlock('Leyla Weder', '/img/avatar.png', 0)
-  renderSearchFormBlock()
+
+  renderSearchFormBlock(dateEntery, dateExit)
+  const innerDate = (<HTMLInputElement>document.querySelector('#check-in-date'))
+  innerDate.addEventListener('change', () => {
+    dateEntery = innerDate.value
+  })
+  const outDate = (<HTMLInputElement>document.querySelector('#check-out-date'))
+  outDate.addEventListener('change', () => {
+    dateExit = outDate.value
+  })
+
   renderSearchStubBlock()
   renderToast(
     { text: 'Это пример уведомления. Используйте его при необходимости', type: 'success' },
